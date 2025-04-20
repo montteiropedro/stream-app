823ac8314b79cd289714e6c9c100d43832956bf3
matheusbarros <matheus53barros@hotmail.com>
2025-04-20 12:52:22 -0300

    wip: add hover

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import logo from "/logo.svg"
+
 function Navbar() {
     return (
         <nav className="bg-black-main text-white h-24 flex items-center justify-between">
-            <div>
-                STREAM APP
+            <div className="flex items-center gap-4">
+                <img src={logo} alt="Logo" className="h-12 ml-4" />
+                <span className="text-3xl font-[Monoton]">STREAM APP</span>
             </div>
 
-            <ul>
-                <li>Following</li>
-                <li><i className="fa-solid fa-magnifying-glass"></i></li>
+            <ul className="flex items-center gap-2.5 text-base font-bold">
+                <li className="px-4 h-10 flex items-center justify-center rounded-lg hover:bg-red-main hover:text-red-500 transition duration-400 ease-in-out"><a href="/">Following</a></li>
+                <li className="size-10 flex items-center justify-center rounded-lg hover:bg-red-main hover:text-red-500 transition duration-400 ease-in-out"><FontAwesomeIcon icon={faMagnifyingGlass} /></li>
+                <li className="px-4 h-10 flex items-center justify-center rounded-lg hover:bg-red-main hover:text-red-500 transition duration-400 ease-in-out"><a href="/">Browse</a></li>
             </ul>
+
+            <div className="user">
+
+            </div>
         </nav>
     )
 }
